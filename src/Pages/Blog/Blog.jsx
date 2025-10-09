289597aa22209189f9bbbaddd5967c0293f1bd84
asadulislam <asadulislam0288@gmail.com>
2025-10-09 20:46:40 +0600

    added by unstall

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
-
+import iR from "../../assets/icon-ratings.png";
+import { toast } from "react-toastify";
+import dI from "../../assets/icon-downloads.png";
 const Blog = () => {
   const [installData, setInstallData] = useState([]);
   const [sortData, setSortData] = useState("none");
@@ -19,6 +21,24 @@ const Blog = () => {
     }
   };
 
+  // no found
+
+  if (!installData.length)
+    return (
+      <p className="text-center text-xl font-semibold text-gray-500 mt-6 mb-4">
+        🚫 No App Installed 😢
+      </p>
+    );
+
+  // App Unstall
+  const handleRemove = (id) => {
+    const installData = JSON.parse(localStorage.getItem("install"));
+    let updateData = installData.filter((p) => p.id !== id);
+    setInstallData(updateData);
+    toast("App Uninstall Successful! ✅");
+    localStorage.setItem("install", JSON.stringify(updateData));
+  };
+  // end
   return (
     <div className="md:w-[1100px] mx-auto mt-8 mb-8 p-2 md:m-4 md:p-4">
       <div className="flex flex-col text-center justify-center">
@@ -49,7 +69,7 @@ const Blog = () => {
       {sortItemData().map((p) => (
         <div
           key={p.id}
-          className="flex justify-between items-center mt-6 bg-gray-300 p-4 md:m-4 rounded-2xl shadow-xl"
+          className="flex justify-between items-center mt-6 bg-gray-300 p-1 md:m-4 rounded-2xl shadow-xl"
         >
           <div className="flex gap-1 md:gap-8 p-2 items-center">
             <div>
@@ -62,15 +82,26 @@ const Blog = () => {
             <div>
               <h1 className="p-2 font-semibold text-xl">{p.title}</h1>
               {/* 3 */}
-              <div className="flex gap-6 p-2">
-                <p>{p.downloads}</p>
-                <p>{p.ratingAvg}</p>
-                <p>{p.size}MB</p>
+              <div className="flex gap-2 md:gap-6 p-2 font-semibold text-[18px]">
+                <div className="flex text-[#00D390] gap-2">
+                  <img className="w-[20px]" src={dI} alt="" />
+                  <p> {p.downloads}</p>
+                </div>
+                <div className="flex text-[#FF8811] gap-2">
+                  <img className="w-[20px]" src={iR} alt="" />
+                  <p>{p.ratingAvg}</p>
+                </div>
+                <p className="text-[#627382]">{p.size}MB</p>
               </div>
             </div>
           </div>
           <div>
-            <button className="btn btn-primary">Uninstall</button>
+            <button
+              onClick={() => handleRemove(p.id)}
+              className="btn w-[50px] text-[10px] md:text-[18px] md:w-[100px] bg-[#00D390] text-white"
+            >
+              Uninstall
+            </button>
           </div>
         </div>
       ))}
